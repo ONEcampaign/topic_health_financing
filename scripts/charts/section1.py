55a3e434df3045b19e95e576934501197fa7455c
Jorge Rivera <jorge.rivera@one.org>
2023-02-22 17:14:00 +0100

    sections

--- scripts/charts/section1.py
+++ scripts/charts/section1.py
@@ -1,79 +1,23 @@
+from functools import partial
+
 import pandas as pd
 
 from scripts.analysis.data_versions import read_spending_data_versions
+from scripts.charts.common import (
+    combine_income_countries,
+    get_version,
+    per_capita_by_income,
+    total_by_income,
+)
 from scripts.tools import (
     value2gdp_share,
     value2gdp_share_group,
-    value2pc_group,
-    value_total_group,
 )
-
 
 SPENDING = read_spending_data_versions(dataset_name="health_spending")
 
 
-def get_spending_version(version: str) -> pd.DataFrame:
-    columns = ["year", "iso_code", "income_group", "country_name", "value"]
-    year_filter = "year.dt.year <= 2020"
-    other_filters = "iso_code != 'VEN'"
-    income_names = {
-        "High": "High income",
-        "Low": "Low income",
-        "Lower-middle": "Lower-middle income",
-        "Upper-middle": "Upper-middle income",
-    }
-
-    return (
-        SPENDING[version]
-        .query(year_filter)
-        .query(other_filters)
-        .replace({"income_group": income_names})
-        .filter(columns, axis=1)
-    )
-
-
-def per_capita_by_income(spending: pd.DataFrame) -> pd.DataFrame:
-    return (
-        value2pc_group(
-            data=spending,
-            group_by=["year", "income_group"],
-            value_column="value",
-        )
-        .sort_values(["year", "income_group"])
-        .reset_index(drop=True)
-    )
-
-
-def total_by_income(spending: pd.DataFrame) -> pd.DataFrame:
-    return (
-        value_total_group(
-            data=spending,
-            group_by=["year", "income_group"],
-            value_column="value",
-        )
-        .sort_values(["year", "income_group"])
-        .reset_index(drop=True)
-    )
-
-
-def combine_income_countries(
-    income: pd.DataFrame, country: pd.DataFrame
-) -> pd.DataFrame:
-
-    combined = pd.concat([income, country], ignore_index=True)
-
-    # series
-    combined["series"] = combined.country_name.fillna(combined.income_group)
-
-    # order
-    order = combined["series"].drop_duplicates().tolist()
-
-    # reshape
-    return (
-        combined.pivot(index=["year"], columns="series", values="value")
-        .filter(order, axis=1)
-        .reset_index(drop=False)
-    )
+get_spending_version = partial(get_version, versions_dict=SPENDING)
 
 
 def pipeline() -> None:
