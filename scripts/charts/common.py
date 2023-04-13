416644f3ff35e0bb5688fbe3d572c3f7738c5cde
Jorge Rivera <jorge.rivera@one.org>
2023-04-13 15:23:49 -0600

    overview charts 1 and 2

--- scripts/charts/common.py
+++ scripts/charts/common.py
@@ -164,3 +164,45 @@ def get_version(
         )
         .filter(columns, axis=1)
     )
+
+
+def df_to_key_number(
+    df: pd.DataFrame,
+    indicator_name: str,
+    id_column: str,
+    value_columns: str | list[str],
+) -> dict:
+    if isinstance(value_columns, str):
+        value_columns = [value_columns]
+
+    return (
+        df.assign(indicator=indicator_name)
+        .filter(["indicator", id_column] + value_columns, axis=1)
+        .groupby(["indicator"])
+        .apply(
+            lambda x: x.set_index(id_column)[value_columns]
+            .astype(str)
+            .to_dict(orient="index")
+        )
+        .to_dict()
+    )
+
+
+def update_key_number(path: str, new_dict: dict) -> None:
+    """Update a key number json by updating it with a new dictionary"""
+    import os
+    import json
+
+    # Check if the file exists, if not create
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump({}, f)
+
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    for k in new_dict.keys():
+        data[k] = new_dict[k]
+
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
