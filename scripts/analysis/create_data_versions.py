bd14ac6488749d255443ca47a0c01a38eb346e6c
Jorge Rivera <jorge.rivera@one.org>
2023-12-12 18:05:35 +0100

    Refactor data extraction functions and modify data saving pipeline
    The code refactors the data extraction functions in the `download_indicators.py` script, removing the use of partial functions. It also modifies the `save_data_versions` function in the `create_data_versions.py` script to restrict data based on certain conditions. These changes are aimed at making the data extraction and saving processes more straightforward and adapting them to current data structure and requirements.

--- scripts/analysis/create_data_versions.py
+++ scripts/analysis/create_data_versions.py
@@ -9,14 +9,7 @@ from scripts.analysis.download_indicators import (
     get_health_exp_by_function,
     get_health_exp_by_source,
 )
-from scripts.tools import (
-    lcu2gdp,
-    lcu2usd_constant,
-    lcu2usd_current,
-    mn2units,
-    value2pc,
-    year2date,
-)
+from scripts.tools import value2pc, year2date
 
 # Function to get the overall spending data in local currency units.
 health_exp_current_lcu = partial(current_lcu_data, get_current_health_exp)
@@ -52,26 +45,35 @@ health_exp_by_disease_current_lcu = partial(
 )
 
 
-def save_data_versions(spending_function: callable, dataset_name: str) -> None:
+def save_data_versions(
+    spending_function: callable, dataset_name: str, additional_filter: dict = None
+) -> None:
     """Save versions of the LCU data"""
     # Local currency units
-    data = {"lcu": spending_function().pipe(year2date).pipe(mn2units)}
+    if additional_filter is None:
+        kwargs = {}
+    else:
+        kwargs = {"additional_filter": additional_filter}
 
-    # Share of GDP
-    data["gdp_share"] = data["lcu"].pipe(lcu2gdp)
+    full_data = spending_function(**kwargs).pipe(year2date)
 
-    # USD current
-    data["usd_current"] = data["lcu"].pipe(lcu2usd_current).assign(units="USD")
-
-    # USD constant
-    data["usd_constant"] = (
-        data["lcu"]
-        .pipe(lcu2usd_constant)
-        .assign(units=f"{config.CONSTANT_YEAR} constant USD")
-    )
-
-    # USD constant per capita
-    data["usd_constant_pc"] = data["usd_constant"].pipe(value2pc).reset_index(drop=True)
+    data = {
+        "lcu": full_data.loc[lambda d: d.units == "current LCU"].assign(
+            units="Current LCU"
+        ),
+        "usd_current": full_data.loc[lambda d: d.units == "current USD"].assign(
+            units="USD"
+        ),
+        "usd_constant": full_data.loc[lambda d: d.units == "constant USD"].assign(
+            units=f"{config.CONSTANT_YEAR} constant USD"
+        ),
+        "gdp_share": full_data.loc[lambda d: d.units == "percent of GDP"].assign(
+            units="share of GDP"
+        ),
+        "usd_constant_pc": full_data.loc[lambda d: d.units == "constant USD"].pipe(
+            value2pc
+        ),
+    }
 
     # Save
     for key, value in data.items():
@@ -81,32 +83,48 @@ def save_data_versions(spending_function: callable, dataset_name: str) -> None:
 def save_data_pipeline() -> None:
     # Overall health spending
     save_data_versions(
-        spending_function=health_exp_current_lcu, dataset_name="health_spending"
+        spending_function=get_current_health_exp, dataset_name="health_spending"
     )
 
     # COVID-19 spending
     save_data_versions(
-        spending_function=covid_exp_current_lcu, dataset_name="covid_spending"
+        spending_function=get_health_exp_by_function,
+        dataset_name="covid_spending",
+        additional_filter={
+            "dimension_1": "COVID-19 spending",
+            "source": None,
+            "dimension_2": None,
+            "dimension_3": None,
+        },
     )
 
     # Government spending
     save_data_versions(
-        spending_function=gov_total_exp_current_lcu, dataset_name="gov_spending"
+        spending_function=get_health_exp_by_source,
+        additional_filter={"source": "domestic general government"},
+        dataset_name="gov_spending",
     )
 
     # Spending by source
     save_data_versions(
-        spending_function=health_exp_by_source_current_lcu,
+        spending_function=get_health_exp_by_source,
         dataset_name="health_spending_by_source",
     )
 
     # Out-of-pocket spending
     save_data_versions(
-        spending_function=health_exp_oop, dataset_name="health_spending_oop"
+        spending_function=get_health_exp_by_financing_scheme,
+        additional_filter={"dimension_1": "Household out-of-pocket payment"},
+        dataset_name="health_spending_oop",
     )
 
     # Spending by disease
     save_data_versions(
-        spending_function=health_exp_by_disease_current_lcu,
+        spending_function=get_health_exp_by_disease,
+        additional_filter={"source": None, "dimension_2": None},
         dataset_name="health_spending_by_disease",
     )
+
+
+if __name__ == "__main__":
+    save_data_pipeline()
